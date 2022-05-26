ad8346704f3d5d6cb6684f89d6addebbbfe95e9a
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2022-05-26 15:01:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 
 all_movies = []
 
-with open('movies.csv') as f:
+with open('movies.csv',encoding="utf-8") as f:
     reader = csv.reader(f)
     data = list(reader)
     all_movies = data[1:]
